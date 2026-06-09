a99ec00ec0bd2c88872aa25f3326a3e54c402bfa
jy <juyoun919@gmail.com>
2026-06-10 00:33:35 +0900

    feat: categoryEntity추가 및 gitignore 설정

---
+++ backend/src/main/java/com/didyou/backend/domain/CategoriesEntity.java
@@ -0,0 +1,35 @@
+package com.didyou.backend.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table( name = "categories")
+public class CategoriesEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private RoomEntity room;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+//    #000000형식으로 표현 예정
+    @Column(nullable = false, length = 7)
+    private String color;
+
+
+
+}
